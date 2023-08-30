89893c9f2cfd4c1511b1fe1cc9da9ff5da7ded29
David Gabanic <david.gabanic@ejgallo.com>
2023-08-30 16:47:50 -0400

    Implemented framework for page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalComponent } from './pages/personal/personal.component';
+import { ProfessionalComponent } from './pages/professional/professional.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'home',   component: HomeComponent },
+  { path: 'personal',   component: PersonalComponent },
+  { path: 'professional',   component: ProfessionalComponent },
+  { path: '**', redirectTo: '/home' }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalComponent } from './pages/personal/personal.component';
+import { ProfessionalComponent } from './pages/professional/professional.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PersonalComponent,
+    ProfessionalComponent
   ],
   imports: [
     BrowserModule,
